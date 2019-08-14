6e0cd047c95a092b7e8354ad4e91f1602d5b29c3
New <qy_liang@yahoo.com>
2019-08-14 21:02:34 +0800

    switch to file

--- ui/static_apple_association.go
+++ ui/static_apple_association.go
@@ -2,33 +2,30 @@
 // Use of this source code is governed by the Apache 2.0
 // license that can be found in the LICENSE file.
 
+
 package ui // import "miniflux.app/ui"
 
 import (
 	"net/http"
+	"time"
 
 	"miniflux.app/http/request"
-	"miniflux.app/http/response/json"
-	"miniflux.app/http/route"
-	"miniflux.app/model"
+	"miniflux.app/http/response"
+	"miniflux.app/http/response/html"
+	"miniflux.app/ui/static"
 )
 
 func (h *handler) showAppleAppSiteAssociation(w http.ResponseWriter, r *http.Request) {
-	type siteIdentifiers struct {
-		Apps       []string            `json:"apps"`
+	filename := request.RouteStringParam(r, "filename")
+	etag, found := static.BinariesChecksums[filename]
+	if !found {
+		html.NotFound(w, r)
+		return
 	}
 
-	type credentials struct {
-		Webcredentials	siteIdentifiers	`json:"webcredentials"`
-	}
-
-	result := &credentials{
-		Webcredentials: siteIdentifiers{
-			Apps: []string{
-				"V6CP2Z4H8Q.com.NewMobileWay.Geed",
-			},
-		},
-	}
-
-	json.OK(w, r, result)
+	response.New(w, r).WithCaching(etag, 48*time.Hour, func(b *response.Builder) {
+		b.WithHeader("Content-Type", "application/json; charset=utf-8")
+		b.WithBody(static.binaries[filename])
+		b.Write()
+	})
 }
