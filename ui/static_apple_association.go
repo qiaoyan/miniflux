3cb41763d33ff1c06f8c7a66bc2cd7897119b4df
New <qy_liang@yahoo.com>
2019-08-14 21:25:41 +0800

    back to json

--- ui/static_apple_association.go
+++ ui/static_apple_association.go
@@ -7,25 +7,35 @@ package ui // import "miniflux.app/ui"
 
 import (
 	"net/http"
-	"time"
 
 	"miniflux.app/http/request"
-	"miniflux.app/http/response"
-	"miniflux.app/http/response/html"
-	"miniflux.app/ui/static"
+	"miniflux.app/http/response/json"
+	"miniflux.app/http/route"
+	"miniflux.app/model"
 )
 
 func (h *handler) showAppleAppSiteAssociation(w http.ResponseWriter, r *http.Request) {
-	filename := request.RouteStringParam(r, "filename")
-	etag, found := static.BinariesChecksums[filename]
-	if !found {
-		html.NotFound(w, r)
-		return
+	type siteIdentifiers struct {
+		Apps []string `json:"apps"`
 	}
 
-	response.New(w, r).WithCaching(etag, 48*time.Hour, func(b *response.Builder) {
-		b.WithHeader("Content-Type", "application/json; charset=utf-8")
-		b.WithBody(static.binaries[filename])
-		b.Write()
-	})
+	type credentials struct {
+		Webcredentials            siteIdentifiers            `json:"webcredentials"`
+	}
+
+	// siteIdentifiers := &siteIdentifiers{
+	// 	Apps: []string{
+	// 		"V6CP2Z4H8Q.com.NewMobileWay.Geed",
+	// 	},
+	// }
+
+	result := &credentials{
+		Webcredentials: siteIdentifiers{
+Apps: []string{
+	"V6CP2Z4H8Q.com.NewMobileWay.Geed",
+},
+		},
+	}
+
+	json.OK(w, r, result)
 }
